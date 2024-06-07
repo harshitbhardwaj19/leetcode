88d825cb2e37a0a3d3d916cc5d8708c6b8981176
harshitbhardwaj19 <38621210+harshitbhardwaj19@users.noreply.github.com>
2024-06-07 17:02:14 +0530

    Time: 48 ms (34.30%) | Memory: 56.1 MB (22.57%) - LeetSync

---
+++ 648-replace-words/replace-words.java
@@ -0,0 +1,57 @@
+class Solution {
+    Trie root;
+
+    public String replaceWords(List<String> dictionary, String sentence) {
+        root = new Trie();
+        for (String word : dictionary) {
+            buildTrie(root, word, 0);
+        }
+        String sol = "";
+        String[] strArr = sentence.split(" ");
+        for(String word : strArr){
+            String rootWord = getRootWord(root, word, 0);
+            sol = sol + " " + rootWord;
+        }
+        return sol.trim();
+    }
+
+    public String getRootWord(Trie root, String word, int index) {
+        if (index == word.length()) {
+            return word;
+        }
+        char ch = word.charAt(index);
+        if (root.children[ch - 'a'] == null) {
+            return word;
+        }
+        root = root.children[ch - 'a'];
+        if(root.isEnding){
+            return word.substring(0, index+1);
+        }
+        return getRootWord(root, word, index+1);
+    }
+
+    public void buildTrie(Trie root, String word, int index) {
+        char ch = word.charAt(index);
+        if (root.children[ch - 'a'] == null) {
+            root.children[ch - 'a'] = new Trie();
+        }
+
+        root = root.children[ch - 'a'];
+
+        if (index == word.length()-1) {
+            root.isEnding = true;
+            return;
+        }
+
+        buildTrie(root, word, index + 1);
+    }
+}
+
+class Trie {
+    Trie children[];
+    boolean isEnding;
+
+    Trie() {
+        children = new Trie[26];
+    }
+}
