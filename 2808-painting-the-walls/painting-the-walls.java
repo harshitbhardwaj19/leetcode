95add7d44a6df61f503bd578de445fd0fb0ed88b
harshitbhardwaj19 <38621210+harshitbhardwaj19@users.noreply.github.com>
2024-06-13 00:58:50 +0530

    Time: 59 ms (25.53%) | Memory: 47.2 MB (30.64%) - LeetSync

--- 2808-painting-the-walls/painting-the-walls.java
+++ 2808-painting-the-walls/painting-the-walls.java
@@ -1,28 +1,28 @@
 class Solution {
     int n;
-    int dp[][];
+    long dp[][];
 
     public int paintWalls(int[] cost, int[] time) {
         n = cost.length;
-        dp = new int[n+1][n+1];
-        for (int arr[] : dp) {
+        dp = new long[n+1][n+1];
+        for (long arr[] : dp) {
             Arrays.fill(arr, -1);
         }
-        return paintWalls(cost, time, 0, n);
+        return (int) paintWalls(cost, time, 0, n);
     }
 
-    public int paintWalls(int[] cost, int[] time, int index, int remaining) {
+    public long paintWalls(int[] cost, int[] time, int index, int remaining) {
         if (remaining <= 0) {
             return 0;
         }
         if (index >= n) {
-            return 1000000000;
+            return Integer.MAX_VALUE;
         }
         if(dp[index][remaining] != -1 ){
             return dp[index][remaining];
         }
-        int pick = cost[index] + paintWalls(cost, time, index + 1, remaining - 1 - time[index]);
-        int notPick = paintWalls(cost, time, index + 1, remaining);
+        long pick = cost[index] + paintWalls(cost, time, index + 1, remaining - 1 - time[index]);
+        long notPick = paintWalls(cost, time, index + 1, remaining);
 
         return dp[index][remaining] = Math.min(pick, notPick);
     }
