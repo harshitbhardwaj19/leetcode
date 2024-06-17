30755b5171ea0c81811415fc8067ec694f13a1f5
harshitbhardwaj19 <38621210+harshitbhardwaj19@users.noreply.github.com>
2024-06-18 00:35:09 +0530

    Time: 5 ms (5.62%) | Memory: 43.5 MB (90.67%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.java
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.java
@@ -7,7 +7,6 @@ class Solution {
             i = i >> 1;
             j = j >> 1;
             count++;
-            System.out.println("Iteration " + count + ", i = " + i + ", j = " + j);
         }
         if( (i ==0 && j!=0) || (i !=0 && j==0)){
             return 0;
@@ -16,9 +15,7 @@ class Solution {
 
         int ans = 0;
         while(count >= 0){
-            System.out.println(count+"  "+((left>>count) & 1) +"  "+((right>>count) & 1));
             if( ((left>>count) & 1) !=  ((right>>count) & 1)){
-                System.out.println("Early exit at count " + count + ", left >> " + count + " = " + (left >> count) + ", right >> " + count + " = " + (right >> count));
                 return ans;
             }
             ans += ((left>>count) & 1)<<count;
