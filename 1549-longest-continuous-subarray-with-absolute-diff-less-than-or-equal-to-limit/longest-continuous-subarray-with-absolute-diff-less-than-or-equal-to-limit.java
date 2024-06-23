d32e6255f41a8bb43965abd0910420124d05282c
harshitbhardwaj19 <38621210+harshitbhardwaj19@users.noreply.github.com>
2024-06-23 17:13:16 +0530

    Time: 22 ms (98.83%) | Memory: 58.2 MB (8.49%) - LeetSync

--- 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1549-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,16 +1,39 @@
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
+        LinkedList<Integer> increase = new LinkedList<>();
+        LinkedList<Integer> decrease = new LinkedList<>();
+
+        int max = 0;
         int left = 0;
-        TreeSet<Integer> set = new TreeSet<>((a, b) -> nums[a] == nums[b] ? a - b : nums[a] - nums[b]);
-        set.add(0);
-        int res = 1;
-        for (int right = 1; right < nums.length; right++) {
-            set.add(right);
-            while (nums[set.last()] - nums[set.first()] > limit) {
-                set.remove(left++);
+
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+
+            while (increase.size() > 0 && n < increase.getLast()) {
+                increase.removeLast();
             }
-            res = Math.max(res, right - left + 1);
+            increase.add(n);
+
+            while (decrease.size() > 0 && n > decrease.getLast()) {
+                decrease.removeLast();
+            }
+
+            decrease.add(n);
+
+            while (decrease.getFirst() - increase.getFirst() > limit) {
+                if (nums[left] == decrease.getFirst()) {
+                    decrease.removeFirst();
+                }
+                if (nums[left] == increase.getFirst()) {
+                    increase.removeFirst();
+                }
+                left++;
+            }
+
+            int size = i - left + 1;
+            max = Math.max(max, size);
         }
-        return res;
+
+        return max;
     }
 }
