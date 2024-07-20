b31b60eda33df5a7032fc13f23eaf6e9be661cae
harshitbhardwaj19 <38621210+harshitbhardwaj19@users.noreply.github.com>
2024-07-20 16:57:40 +0530

    Time: 7 ms (52.59%) | Memory: 52.9 MB (53.39%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
@@ -1,49 +1,14 @@
 class Solution {
-    int m;
-    int n;
-
-    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
-        m = rowSum.length;
-        n = colSum.length;
-        int matrix[][] = new int[m][n];
-
-        int temp = Math.min(rowSum[0], colSum[0]);
-        for (int i = temp; i >= 0; i--) {
-            matrix[0][0] = i;
-            rowSum[0] -= i;
-            colSum[0] -= i;
-            if (isPossible(1, matrix, rowSum, colSum)) {
-                return matrix;
+    public int[][] restoreMatrix(int[] row, int[] col) {
+        int m = row.length, n = col.length;
+        int[][] A = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0 ; j < n; ++j) {
+                A[i][j] = Math.min(row[i], col[j]);
+                row[i] -= A[i][j];
+                col[j] -= A[i][j];
             }
-            rowSum[0] += i;
-            colSum[0] += i;
         }
-        return null;
-    }
-
-    public boolean isPossible(int index, int matrix[][], int[] rowSum, int[] colSum) {
-        int row = index / n;
-        int col = index % n;
-
-        if(index >= m*n){
-            return true;
-        }
-
-        if (rowSum[row] < 0 || colSum[col] < 0) {
-            return false;
-        }
-
-        int temp = Math.min(rowSum[row], colSum[col]);
-        for (int i = temp; i >= 0; i--) {
-            matrix[row][col] = i;
-            rowSum[row] -= i;
-            colSum[col] -= i;
-            if (isPossible(index + 1, matrix, rowSum, colSum)) {
-                return true;
-            }
-            rowSum[row] += i;
-            colSum[col] += i;
-        }
-        return false;
+        return A;
     }
 }
