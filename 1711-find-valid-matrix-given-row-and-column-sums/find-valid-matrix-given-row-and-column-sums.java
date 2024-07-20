d4128b42253a9cd9a941b3f73ffe373fcef81402
harshitbhardwaj19 <38621210+harshitbhardwaj19@users.noreply.github.com>
2024-07-20 16:43:23 +0530

    Time: 60 ms (5.98%) | Memory: 87.3 MB (6.77%) - LeetSync

---
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
@@ -0,0 +1,56 @@
+class Solution {
+    int m;
+    int n;
+
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        m = rowSum.length;
+        n = colSum.length;
+        int matrix[][] = new int[m][n];
+
+        int temp = Math.min(rowSum[0], colSum[0]);
+        for (int i = temp; i >= 0; i--) {
+            matrix[0][0] = i;
+            rowSum[0] -= i;
+            colSum[0] -= i;
+            if (isPossible(1, matrix, rowSum, colSum)) {
+                return matrix;
+            }
+            rowSum[0] += i;
+            colSum[0] += i;
+        }
+        return null;
+    }
+
+    public boolean isPossible(int index, int matrix[][], int[] rowSum, int[] colSum) {
+        // System.out.println(matrix);
+        // System.out.println(rowSum);
+        // System.out.println(colSum);
+        int row = index / n;
+        int col = index % n;
+
+        // System.out.println(index+"  "+row+"  "+col);
+        if(index >= m*n){
+            return true;
+        }
+        // if(row>=m || col>=n){
+        //     return false;
+        // }
+
+        if (rowSum[row] < 0 || colSum[col] < 0) {
+            return false;
+        }
+
+        int temp = Math.min(rowSum[row], colSum[col]);
+        for (int i = temp; i >= 0; i--) {
+            matrix[row][col] = i;
+            rowSum[row] -= i;
+            colSum[col] -= i;
+            if (isPossible(index + 1, matrix, rowSum, colSum)) {
+                return true;
+            }
+            rowSum[row] += i;
+            colSum[col] += i;
+        }
+        return false;
+    }
+}
