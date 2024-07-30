674299adc4f6fea0bf96105323227d88a1a519bd
harshitbhardwaj19 <38621210+harshitbhardwaj19@users.noreply.github.com>
2024-07-30 21:04:46 +0530

    Time: 18 ms (99.16%) | Memory: 45 MB (91.64%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,19 +1,20 @@
 class Solution {
     public int minimumDeletions(String s) {
-        int dp[][] = new int[s.length()][2];
-        dp[0][0] = s.charAt(0) == 'a' ? 0 : 1;
-        dp[0][1] = s.charAt(0) == 'a' ? 1 : 0;
-
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == 'a') {
-                dp[i][0] = dp[i - 1][0];
-                dp[i][1] = Math.min(dp[i - 1][0] + 1, dp[i - 1][1] + 1);
+        int ans = 0;
+        int countB = 0;
+        for (char c : s.toCharArray()) {
+            if (c == 'b') {
+                countB++;
             } else {
-                dp[i][0] = dp[i - 1][0] + 1;
-                dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][1]);
+                // is 'a'
+                // if countB is zero, 
+                //     then ans will be 0, because we do not need to remove anything
+                // in the case where countB is no longer 0
+                //     then we just need to remove the min(countB, ans + 1) 
+                //     [this +1 represents the 'a' we encounter in this iteration + ans (those to be removed in previous iteration)]
+                ans = Math.min(ans + 1, countB);
             }
         }
-
-        return Math.min(dp[s.length()-1][0], dp[s.length()-1][1]);
+        return ans;
     }
 }
