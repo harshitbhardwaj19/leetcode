4bf818e1a42b4d4373c47ccbc6832d3678e82674
harshitbhardwaj19 <38621210+harshitbhardwaj19@users.noreply.github.com>
2024-08-19 19:54:51 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (86.97%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -1,17 +1,18 @@
 class Solution {
-        public int minSteps(int n) {
-        int[] dp = new int[n+1];
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = i;
-            for (int j = i-1; j > 1; j--) {
-                if (i % j == 0) {
-                    dp[i] = dp[j] + (i/j);
-                    break;
-                }
-                
+    public int minSteps(int n) {
+        if (n == 1) return 0;
+        
+        int steps = 0;
+        int factor = 2;
+        
+        while (n > 1) {
+            while (n % factor == 0) {
+                steps += factor;
+                n /= factor;
             }
+            factor++;
         }
-        return dp[n];
+        
+        return steps;
     }
 }
