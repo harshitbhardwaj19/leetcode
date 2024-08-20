697edd50d3a99ed04a494380cd892a2b33459d82
harshitbhardwaj19 <38621210+harshitbhardwaj19@users.noreply.github.com>
2024-08-20 21:27:37 +0530

    Time: 11 ms (14.54%) | Memory: 54.5 MB (8.13%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,30 +1,19 @@
 class Solution {
+    Integer[][] dp = new Integer[501][501];
     public boolean stoneGame(int[] piles) {
-        int start = 0;
-        int end = piles.length - 1;
-
-        int alice=0;
-        int bob=0;
-
-        boolean aTurn = true;
-
-        while(start<=end){
-            if(piles[start] > piles[end]){
-                if(aTurn){
-                    alice+=piles[start];
-                } else {
-                    bob+=piles[start];
-                }
-                start++;
-            } else {
-                if(aTurn){
-                    alice+=piles[start];
-                } else {
-                    bob+=piles[start];
-                }
-                end--;
-            }
-        }
-        return alice>bob;
+        return recursion( 0,piles.length-1,piles) > 0;
+    }
+    
+    public int recursion( int start, int end, int[] piles )
+    {
+        if( start > end )
+            return 0;
+        if( dp[start][end] != null )
+            return dp[start][end];
+			
+        int option1 = piles[start]-recursion( start+1,end,piles);
+        int option2 = piles[end]-recursion(start,end-1,piles);
+        
+        return dp[start][end] = Math.max(option1,option2);
     }
 }
