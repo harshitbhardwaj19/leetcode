388a3daac9be19f8c555ea020c1bcca9a37a0fe2
harshitbhardwaj19 <38621210+harshitbhardwaj19@users.noreply.github.com>
2024-08-29 00:28:46 +0530

    Time: 28 ms (34.79%) | Memory: 73.1 MB (95.06%) - LeetSync

---
+++ 2035-count-sub-islands/count-sub-islands.java
@@ -0,0 +1,43 @@
+class Solution {
+    int m;
+    int n;
+
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        m = grid1.length;
+        n = grid1[0].length;
+        boolean visited[][] = new boolean[m][n];
+
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid2[i][j] == 1 && !visited[i][j]) {
+                    // System.out.println(i+"  "+j+"##########");
+                    if (helper(grid1, grid2, i, j, visited)) {
+                        // System.out.println(i+"  "+j+" ***********");
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public boolean helper(int[][] grid1, int[][] grid2, int i, int j, boolean visited[][]) {
+        // System.out.println(i+"  "+j);
+        if (i < 0 || j < 0 || i >= m || j >= n || visited[i][j] || grid2[i][j] == 0) {
+            return true;
+        }
+
+        boolean sub = true;
+        if (grid1[i][j] == 0) {
+            sub = false;
+        }
+        visited[i][j] = true;
+
+        boolean b1 = helper(grid1, grid2, i + 1, j, visited);
+        boolean b2 = helper(grid1, grid2, i, j + 1, visited);
+        boolean b3 = helper(grid1, grid2, i - 1, j, visited);
+        boolean b4 = helper(grid1, grid2, i, j - 1, visited); 
+        return b1 && b2 && b3 && b4 && sub;
+    }
+}
