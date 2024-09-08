ffd2651165704e909bd46d83205acae2d91bc66c
harshitbhardwaj19 <38621210+harshitbhardwaj19@users.noreply.github.com>
2024-09-09 00:16:33 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (94.23%) - LeetSync

---
+++ 725-split-linked-list-in-parts/split-linked-list-in-parts.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        int size = 0;
+        ListNode arr[] = new ListNode[k];
+        ListNode node = head;
+        while(node!=null){
+            size++;
+            node=node.next;
+        }
+        int chunkSize = size/k;
+
+        // System.out.println(size + " "+chunkSize);
+        // System.out.println(size%k);
+
+        int count = 0;
+        node = head;
+        for(int i = 0; i<size%k; i++){
+            arr[count++] = node;
+            for(int j = 0; j<chunkSize; j++){
+                node=node.next;
+            }
+            ListNode next = node.next;
+            node.next = null;
+            node = next;
+        }
+        if(chunkSize == 0){
+            return arr;
+        }
+        int rem = size - (chunkSize+1)*count;
+        // System.out.println(rem);
+        for(int i = 0; i<rem/chunkSize; i++){
+            arr[count++] = node;
+            for(int j = 0; j<chunkSize-1; j++){
+                node=node.next;
+            }
+            ListNode next = node.next;
+            node.next = null;
+            node = next;
+        }
+
+        return arr;
+    }
+}
