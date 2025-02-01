f4b5c31b799926b321a259a336af49c874c234e9
harshitbhardwaj19 <38621210+harshitbhardwaj19@users.noreply.github.com>
2025-02-01 23:35:53 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (99.18%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,15 +1,11 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n==1){
-            return 1;
+        int arr[] = new int[n + 1];
+        arr[1] = 1;
+        arr[0] = 1;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
         }
-        int dp[] = new int[n+1];
-        dp[1]=1;
-        dp[0]=1;
-
-        for(int i = 2; i<=n; i++){
-            dp[i] = dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+        return arr[n];
     }
 }
