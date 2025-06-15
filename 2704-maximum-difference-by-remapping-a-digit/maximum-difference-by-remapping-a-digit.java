de3f8563b48affab39ce601ab6b1a58059c94625
harshitbhardwaj19 <38621210+harshitbhardwaj19@users.noreply.github.com>
2025-06-15 21:06:40 +0530

    Time: 6 ms (14.36%) | Memory: 40.7 MB (56.35%) - LeetSync

---
+++ 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minMaxDifference(int num) {
+        String numStr = num+"";
+        char first = numStr.charAt(0);
+        int pos = 1;
+        while(first=='9' && pos<numStr.length()){
+            first = numStr.charAt(pos);
+            pos++;
+        }
+        String max = numStr.replace(first, '9');
+        String min = numStr.replace(numStr.charAt(0), '0');
+        System.out.println(first);
+        System.out.println(max);
+        System.out.println(min);
+        return Integer.parseInt(max) - Integer.parseInt(min);
+    }
+}
