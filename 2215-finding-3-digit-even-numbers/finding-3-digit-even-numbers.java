3b49cda87710135c20f49de270ca6bf08ca6b13d
harshitbhardwaj19 <38621210+harshitbhardwaj19@users.noreply.github.com>
2025-10-06 00:23:58 +0530

    Time: 255 ms (36.72%) | Memory: 45.5 MB (14.31%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
@@ -4,14 +4,14 @@ class Solution {
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < l; i++) {
             if (digits[i] == 0) {
-                    continue;
+                continue;
             }
             for (int j = 0; j < l; j++) {
                 if (j == i) {
                     continue;
                 }
                 for (int k = 0; k < l; k++) {
-                    if (digits[k]%2 == 1 || k == j || k == i) {
+                    if (digits[k] % 2 == 1 || k == j || k == i) {
                         continue;
                     }
                     set.add(digits[i] * 100 + digits[j] * 10 + digits[k]);
@@ -19,7 +19,7 @@ class Solution {
             }
         }
         int arr[] = new int[set.size()];
-        int i =0;
+        int i = 0;
         for (Integer num : set) {
             arr[i] = num;
             i++;
