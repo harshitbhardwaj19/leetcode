8886e872d339b62f84da880e906f4ce6a0626963
harshitbhardwaj19 <38621210+harshitbhardwaj19@users.noreply.github.com>
2025-10-25 13:52:16 +0530

    Time: 14 ms (86.32%) | Memory: 70.9 MB (95.06%) - LeetSync

--- 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.java
+++ 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.java
@@ -1,37 +1,15 @@
 class Solution {
-    public int maxIncreasingSubarrays(List<Integer> nums) {
-        int l = nums.size();
-        int min = 1;
-        int max = l/2;
-        int ans = 1;
-        while(min<=max){
-            int mid = (min+max)/2;
-            if(hasIncreasingSubarrays(nums,mid)){
-                ans = mid;
-                min = mid+1;
+        public int maxIncreasingSubarrays(List<Integer> A) {
+        int n = A.size(), up = 1, pre_max_up = 0, res = 0;
+        for (int i = 1; i < n; ++i) {
+            if (A.get(i) > A.get(i - 1)) {
+                up++;
             } else {
-                max = mid - 1;
+                pre_max_up = up;
+                up = 1;
             }
+            res = Math.max(res, Math.max(up / 2, Math.min(pre_max_up, up)));
         }
-        return ans;
-    }
-    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
-        int l = nums.size();
-        int arr[] = new int[l];
-        arr[0] = 1;
-        for (int i = 1; i < l; i++) {
-            if ((nums.get(i - 1) < nums.get(i))) {
-                arr[i] = arr[i - 1] + 1;
-            } else {
-                arr[i] = 1;
-            }
-        }
-
-        for (int i = 0; i < l - k; i++) {
-            if (arr[i] >= k && arr[i + k] >= k) {
-                return true;
-            }
-        }
-        return false;
+        return res;
     }
 }
