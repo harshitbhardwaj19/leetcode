e6e4c64d066cb72652a44b12cd01856eb8b7d898
harshitbhardwaj19 <38621210+harshitbhardwaj19@users.noreply.github.com>
2026-01-04 15:07:44 +0530

    Time: 1 ms (77.75%) | Memory: 43.7 MB (50.57%) - LeetSync

--- 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.java
+++ 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.java
@@ -1,33 +1,35 @@
 class Solution {
     public int minDeletionSize(String[] strs) {
-        Set<Integer> set = new HashSet<>();
         int n = strs.length;
-        int l = strs[0].length();
-        int count = 0;
-
-        for (int i = 0; i < n - 1; i++) {
-
-            for (int j = 0; j < l; j++) {
-                char c1 = strs[i].charAt(j);
-                char c2 = strs[i + 1].charAt(j);
-
-                if (set.contains(j)) {
-                    continue;
-                }
-                
-                if (c1 == c2) {
-                    continue;
-                } else if (c1 > c2) {
-                    count++;
-                    set.add(j);
-                    i = -1;
-                    break;
-                } else {
+        int m = strs[0].length();
+        
+        // sorted[i] = true means strs[i] < strs[i+1] is already established
+        boolean[] sorted = new boolean[n - 1];
+        int deletions = 0;
+        
+        for (int col = 0; col < m; col++) {
+            // Check if this column causes any violation
+            boolean needDelete = false;
+            for (int i = 0; i < n - 1; i++) {
+                // Only check pairs that aren't already sorted
+                if (!sorted[i] && strs[i].charAt(col) > strs[i + 1].charAt(col)) {
+                    needDelete = true;
                     break;
                 }
             }
+            
+            if (needDelete) {
+                deletions++;
+            } else {
+                // Keep this column - update sorted status
+                for (int i = 0; i < n - 1; i++) {
+                    if (strs[i].charAt(col) < strs[i + 1].charAt(col)) {
+                        sorted[i] = true;
+                    }
+                }
+            }
         }
-
-        return count;
+        
+        return deletions;
     }
 }
